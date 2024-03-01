2eb067385aad5d24bd96591b76587c472c6b9362
hidenShark <ilushashark@gmail.com>
2024-03-01 03:00:50 +0300

    Finish

--- LoginScreen_MVVM/LoginScreen_MVVM/Views/ViewController.swift
+++ LoginScreen_MVVM/LoginScreen_MVVM/Views/ViewController.swift
@@ -9,21 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var loginTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var statusTextLabel: UILabel!
+    
     var viewModel = ViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         bindViewModel()
     }
+    
+    @IBAction func loginButtonPress(_ sender: UIButton) {
+        viewModel.userButtonPress(login: loginTextField.text ?? "", password: passwordTextField.text ?? "")
+    }
+    
 
     func bindViewModel() {
         viewModel.statusText.bind({ (statusText) in
             DispatchQueue.main.async {
-                self.label.text = statusText
+                self.statusTextLabel.text = statusText
             }
-            
-        }
-        )
+        })
+        viewModel.statusColor.bind({ (statusColor) in
+            DispatchQueue.main.async {
+                self.statusTextLabel.textColor = statusColor
+            }
+        })
     }
 
 }
